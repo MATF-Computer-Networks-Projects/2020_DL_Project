030f4552aec825f4300f8274822c1b2af858796d
lristic <lristic4@yahoo.com>
2021-01-06 23:20:09 +0100

    Create login and register forms and tweak design
    User will now be prompted to login or register
    After registering, user will be redirected to login page
    After logging in, user will be redirected to home page and won't be able to return to login/register pages until logged out
    If logged in, home page will show username and Log Out button
    If user makes any errors while logging in/registering, he will be notified
    Forms have been designed and home page has been retyped

--- dl_project/chat/urls.py
+++ dl_project/chat/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path('chat/<str:room_name>/', views.room, name='room'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
--- dl_project/chat/views.py
+++ dl_project/chat/views.py
@@ -1,9 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from . forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
+    # user = authenticate(request, username=username)
+    # if user is not None:
     return render(request, 'chat/index.html')
+    # else:
+    #     print("Not working")
 
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'User successfully created!') 
+                return redirect('login')
+
+        context = {
+            'form': form
+            }
+        return render(request, 'chat/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = username, password = password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, "Username or password is incorrect")
+                return render(request, 'chat/login.html')
+
+        context = {}
+        return render(request, 'chat/login.html')
+    
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
